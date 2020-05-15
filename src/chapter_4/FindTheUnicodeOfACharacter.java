7f58e180311a055bea8f301cc999ca0a7edce3a2
Динислам <sul.din.tan@gmail.com>
2020-05-15 19:21:17 +0600

    9th of may 2020

---
+++ src/chapter_4/FindTheUnicodeOfACharacter.java
@@ -0,0 +1,18 @@
+package chapter_4;
+
+import java.util.Scanner;
+
+public class FindTheUnicodeOfACharacter {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a character: ");
+        String s = input.nextLine();
+        char ch = s.charAt(0);
+
+
+        System.out.println((int)ch);
+    }
+}
+
+
